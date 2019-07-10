fcc03782c0b2b9375eeecb8c6d22b426db9c55cf
Ankit Kumar Sharma <43175867+AnkitKumarSharma501@users.noreply.github.com>
2019-07-10 12:52:05 +0530

    Navigate our hats with react-router-dom component

--- secobuy/src/App.js
+++ secobuy/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/homepages/homepage.component';
 
+
+const HatsPage = () => (
+  <div>
+    <h1> Hates Page </h1>
+  </div>
+)
+
 function App() {
   return (
-    <div className="">
-      <Homepage />
+    <div>
+      <switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage}/>
+      </switch>
     </div>
   );
 }
