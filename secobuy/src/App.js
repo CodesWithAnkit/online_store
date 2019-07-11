8005d6e2f2e3f6839db58ad0f699618113dd6481
Ankit Kumar Sharma <43175867+AnkitKumarSharma501@users.noreply.github.com>
2019-07-11 13:54:28 +0530

    Adding signIn component and their style with buttom component and their style with some functionality

--- secobuy/src/App.js
+++ secobuy/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/homepages/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx'
-import Header from './components/header/header.component.jsx'
+import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sing-up/sign-in-and-sing-up.component'
+import Header from './components/header/header.component'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
