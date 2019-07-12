e4e62bab06bc30ff9057d3c459f36e742b2dc1bf
Ankit Kumar Sharma <43175867+AnkitKumarSharma501@users.noreply.github.com>
2019-07-12 17:49:37 +0530

    Finally I cure my login data but I done that by the help of tutors file which I clone and test with my file. But it isworking right now...Thanks for the code which is avaible for the help

--- secobuy/src/firebase/firebase.utils.js
+++ secobuy/src/firebase/firebase.utils.js
@@ -12,17 +12,13 @@ const config = {
     appId: "1:815874742482:web:a7ae0858f2361b7d"
 };
 
-
-
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-
-provider.setCustomParameters({ prompt: 'select-account' });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
 
 export default firebase;
